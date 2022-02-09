cce70ea0b4e13f59587fc717ff58a94e0ee06491
Donald Burke <db9962@rit.edu>
2022-02-08 19:20:04 -0500

    HeroController class set up, getProducts() complete.

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/InventoryController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/InventoryController.java
@@ -1,5 +1,52 @@
 package com.estore.api.estoreapi.controller;
 
-public class InventoryController {
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import com.estore.api.estoreapi.model.Product;
+import com.estore.api.estoreapi.persistence.InventoryDAO;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("products")
+public class InventoryController {
+    private static final Logger LOG = Logger.getLogger(InventoryController.class.getName());
+    private InventoryDAO inventoryDao;
+
+    /**
+     * Creates a REST API controller to reponds to requests
+     * 
+     * @param heroDao The {@link InventoryDAO Inventory Data Access Object} to perform CRUD operations
+     * <br>
+     * This dependency is injected by the Spring Framework
+     */
+    public InventoryController(InventoryDAO inventoryDAO) {
+        this.inventoryDao = inventoryDAO;
+    }
+
+    /**
+     * Responds to the GET request for all {@linkplain Product products}
+     * 
+     * @return ResponseEntity with array of {@link Product product} objects (may be empty) and
+     * HTTP status of OK<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @GetMapping("")
+    public ResponseEntity<Product[]> getProducts() {
+        LOG.info("GET /products");
+
+        try {
+            return new ResponseEntity<Product[]>(inventoryDao.getProducts(),HttpStatus.OK);
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
