c9389cc3719065b6da12a525bd3c4c5489cd0482
Alen Van <ajv6049@rit.edu>
2022-02-10 19:13:57 -0500

    Completed createProduct and getProduct in both the controller and the DAO

--- estore-api/src/main/java/com/estore/api/estoreapi/controller/InventoryController.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/InventoryController.java
@@ -11,20 +11,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD
-=======
 /**
  * Handles the REST API requests for the Inventory resource
  * <p>
- * {@literal @}RestController Spring annotation identifies this class as a REST API
+ * {@literal @}RestController Spring annotation identifies this class as a REST
+ * API
  * method handler to the Spring framework
  * 
  * @author Team jadin
  */
 
->>>>>>> 3e483dbcdbe6e952c378130538de07a5eef858a3
 @RestController
 @RequestMapping("products")
 public class InventoryController {
@@ -34,10 +33,10 @@ public class InventoryController {
     /**
      * Creates a REST API controller to reponds to requests
      * 
-     * @param heroDao The {@link InventoryDAO Inventory Data Access Object} to
-     *                perform CRUD operations
-     *                <br>
-     *                This dependency is injected by the Spring Framework
+     * @param inventoryDAO The {@link InventoryDAO Inventory Data Access Object} to
+     *                     perform CRUD operations
+     *                     <br>
+     *                     This dependency is injected by the Spring Framework
      */
     public InventoryController(InventoryDAO inventoryDAO) {
         this.inventoryDao = inventoryDAO;
@@ -64,12 +63,54 @@ public class InventoryController {
     }
 
     /**
+     * Creates a {@link Product} with the provided product object
      * 
+     * @param product The {@link Product} to create
+     * 
+     * @return A ResponseEntity with the newly created {@link Product product} and
+     *         HTTP status CREATED
+     *         A ResponseEntity with HTTP status of CONFLICT if {@link Product
+     *         product} already exists
+     *         A ResposneEntity with HTTP status INTERNAL_SERVER_ERROR otherwise
      */
     @GetMapping("")
-    public ResponseEntity<Product> createProduct(Product newProduct) {
+    public ResponseEntity<Product> createProduct(@RequestParam Product product) {
         try {
-            
+            Product newProduct = inventoryDao.createProduct(product);
+            if (newProduct == null) {
+                return new ResponseEntity<>(HttpStatus.CONFLICT);
+            } else {
+                return new ResponseEntity<Product>(newProduct, HttpStatus.CREATED);
+            }
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Gets a {@link Product} with the provided id
+     * 
+     * @param id An id that is used to get a product that corresponds to that id
+     *           from the inventory
+     * 
+     * @return A ResonseEntity with the {@link Product product} that was obtained
+     *         from the id
+     *         and a HTTP status code OK
+     *         A ResponseEntity with HTTP status of NOT_FOUND if no product was
+     *         found
+     *         A ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @GetMapping("")
+    public ResponseEntity<Product> getProduct(@RequestParam int id) {
+        try {
+            Product productGot = inventoryDao.getProduct(id);
+            if (productGot == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            } else {
+                return new ResponseEntity<Product>(productGot, HttpStatus.OK);
+            }
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
