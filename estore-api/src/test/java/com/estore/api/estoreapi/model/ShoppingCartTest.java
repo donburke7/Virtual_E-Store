5909c7c8e15294b8fb724001efe4093c2cb3de7a
Alen Van <ajv6049@rit.edu>
2022-03-01 20:35:39 -0500

    added tests for shopping cart

--- estore-api/src/test/java/com/estore/api/estoreapi/model/ShoppingCartTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/model/ShoppingCartTest.java
@@ -2,9 +2,26 @@ package com.estore.api.estoreapi.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.TreeMap;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ShoppingCartTest {
+
+    ShoppingCart testShoppingCart;
+    ShoppingCart emptyShoppingCart;
+
+    @BeforeEach
+    public void setup() {
+        TreeMap<Integer, Product> map = new TreeMap();
+        Product expected = new Product("Green Beans", 0, 1, 1.00);
+        map.put(0, expected);
+
+        testShoppingCart = new ShoppingCart(map);
+        emptyShoppingCart = new ShoppingCart(null);
+    }
+
     @Test
     public void testAddProduct() {
         // Setup
@@ -18,4 +35,27 @@ public class ShoppingCartTest {
         // Analysis
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void testRemoveProduct() {
+        TreeMap<Integer, Product> map = new TreeMap();
+
+        Product expected = new Product("Green Beans", 0, 1, 1.00);
+
+        map.put(0, expected);
+
+        ShoppingCart cart = new ShoppingCart(map);
+
+        assertEquals(expected, cart.removeProduct(0));
+
+    }
+
+    @Test
+    public void testClearCart() {
+        testShoppingCart.clearCart();
+
+        assertEquals(0, testShoppingCart.getItems().size());
+
+    }
+
 }
--- estore-api/src/test/java/com/estore/api/estoreapi/model/Users/CustomerTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/model/Users/CustomerTest.java
@@ -2,12 +2,31 @@ package com.estore.api.estoreapi.model.Users;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.TreeMap;
+
 import com.estore.api.estoreapi.model.Product;
 import com.estore.api.estoreapi.model.ShoppingCart;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CustomerTest {
+
+    ShoppingCart testShoppingCart;
+    ShoppingCart emptyShoppingCart;
+
+    @BeforeEach
+    public void setup() {
+
+        TreeMap<Integer, Product> map = new TreeMap();
+        Product expected = new Product("Green Beans", 0, 1, 1.00);
+        map.put(0, expected);
+
+        testShoppingCart = new ShoppingCart(map);
+        emptyShoppingCart = new ShoppingCart(null);
+
+    }
+
     @Test
     public void testAddProduct() {
         // Setup
