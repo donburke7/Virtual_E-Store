fa4f38136dbccd7628935cc46eb529859c2fdb45
Joseph Doros <josephdoros@josephs-air-2.wireless.rit.edu>
2022-03-01 21:03:57 -0500

    Implemented two unit test

--- estore-api/src/test/java/com/estore/api/estoreapi/model/Users/CustomerTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/model/Users/CustomerTest.java
@@ -2,6 +2,9 @@ package com.estore.api.estoreapi.model.Users;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 import com.estore.api.estoreapi.model.Product;
 import com.estore.api.estoreapi.model.ShoppingCart;
 
@@ -22,4 +25,21 @@ public class CustomerTest {
         // Analysis
         assertEquals(expected, actual);
     }
+    @Test
+    public void testRemoveProduct() {
+        // Setup
+        int id = 0;
+        Product nexpected = new Product("Green Beans", id, 500, 2.00);
+        Customer testCustomer = new Customer("Bart");
+
+        // Invoke
+        testCustomer.removeProduct(id);
+        ShoppingCart cart = testCustomer.getCart();
+        Product actual = cart.removeProduct(id); // Returns the product that was removed, null if not
+        Product expected = nexpected.removeProduct(id);
+
+        // Analysis
+        assertEquals(expected, actual);
+    }
+
 }
