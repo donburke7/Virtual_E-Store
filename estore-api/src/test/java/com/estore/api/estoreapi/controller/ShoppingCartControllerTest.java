0d92731645462c07482af8ce7e9b43b738474c9a
Joseph Doros <jd3545@rit.edu>
2022-03-15 07:48:32 -0400

    Implemented all of the tested functions required, will fix easy errors later

--- estore-api/src/test/java/com/estore/api/estoreapi/controller/ShoppingCartControllerTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/controller/ShoppingCartControllerTest.java
@@ -8,6 +8,8 @@ import static org.mockito.Mockito.when;
 import java.io.IOException;
 
 import com.estore.api.estoreapi.model.Product;
+import com.estore.api.estoreapi.model.ShoppingCart;
+import com.estore.api.estoreapi.model.Users.Customer;
 import com.estore.api.estoreapi.persistence.User.ShoppingCartDAO;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -31,11 +33,202 @@ public class ShoppingCartControllerTest {
      * a mock Product DAO
      */
     @BeforeEach
-    public void setupinventoryController() {
+    public void setupShoppingCartController() {
         mockCartDAO = mock(ShoppingCartDAO.class);
         cartController = new ShoppingCartController(mockCartDAO);
     }
+    @Test
+    public void testgetCart() throws IOException { // getProduct may throw IOException
+        // Setup
+        Product product = new Product("Green Bean", 99, 1, 1.0);
+        ShoppingCart cart = new ShoppingCart(null);
+        cart.addProduct(product);
+        Customer customer = new Customer("John Smith",cart);
 
+        // When the same id is passed in, our mock Product DAO will return the Product
+        // object
+        //when(mockCartDAO.getShoppingCart(customer.getUsername(),customer.getCart())).thenReturn(product);
+        when(mockCartDAO.getShoppingCart(customer.getUsername()).thenReturn(customer));
+
+        // Invoke
+        ResponseEntity<Product> response = cartController.getShoppingCart(customer.getUsername());
+
+        // Analyze
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(product, response.getBody());
+    }
+
+    @Test
+    public void testgetCartNotFound() throws Exception { // createProduct may throw IOException
+        // Setup
+        String customerName = "John Smith";
+        // When the same id is passed in, our mock Product DAO will return null,
+        // simulating
+        // no Product found
+        when(mockCartDAO.getShoppingCart(customerName)).thenReturn(null);
+
+        // Invoke
+        ResponseEntity<Product> response = cartController.getShoppingCart(customerName);
+
+        // Analyze
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+    @Test
+    public void testgetShoppingCartHandleException() throws Exception { // createProduct may throw IOException
+        // Setup
+        String customerName = "John Smith";
+        // When getProduct is called on the Mock Product DAO, throw an IOException
+        doThrow(new IOException()).when(mockCartDAO).getShoppingCart(customerName);
+
+        // Invoke
+        ResponseEntity<Product> response = cartController.getShoppingCart(customerName);
+
+        // Analyze
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+    @Test
+    public void testdeleteProduct() throws IOException { // deleteProduct may throw IOException
+        // Setup
+        String customerName = "John Smith";
+        int ProductId = 99;
+        // when deleteProduct is called return true, simulating successful deletion
+        when(mockCartDAO.deleteProduct(customerName, ProductId)).thenReturn(true);
+
+        // Invoke
+        ResponseEntity<Product> response = cartController.deleteProduct(customerName, ProductId);
+
+        // Analyze
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void testdeleteProductNotFound() throws IOException { // deleteProduct may throw IOException
+        // Setup
+        String customerName = "John Smith";
+        int ProductId = 99;
+        // when deleteProduct is called return false, simulating failed deletion
+        when(mockCartDAO.deleteProduct(customerName, ProductId)).thenReturn(false);
+
+        // Invoke
+        ResponseEntity<Product> response = cartController.deleteProduct(customerName, ProductId);
+
+        // Analyze
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    public void testdeleteProductHandleException() throws IOException { // deleteProduct may throw IOException
+        // Setup
+        String customerName = "John Smith";
+        int ProductId = 99;
+        // When deleteProduct is called on the Mock Product DAO, throw an IOException
+        doThrow(new IOException()).when(mockCartDAO).deleteProduct(customerName, ProductId);
+
+        // Invoke
+        ResponseEntity<Product> response = cartController.deleteProduct(customerName, ProductId);
+
+        // Analyze
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+    @Test
+    public void testclearShoppingCart() throws IOException { // deleteProduct may throw IOException
+        // Setup
+        String customerName = "John Smith";
+        int ProductId = 99;
+        // when deleteProduct is called return true, simulating successful deletion
+        when(mockCartDAO.clearShoppingCart(customerName)).thenReturn(true);
+
+        // Invoke
+        ResponseEntity<Product> response = cartController.clearShoppingCart(customerName);
+
+        // Analyze
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void testclearShoppingCartNotFound() throws IOException { // deleteProduct may throw IOException
+        // Setup
+        String customerName = "John Smith";
+        int ProductId = 99;
+        // when deleteProduct is called return false, simulating failed deletion
+        when(mockCartDAO.clearShoppingCart(customerName)).thenReturn(false);
+
+        // Invoke
+        ResponseEntity<Product> response = cartController.clearShoppingCart(customerName);
+
+        // Analyze
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    public void testclearShoppingCartHandleException() throws IOException { // deleteProduct may throw IOException
+        // Setup
+        String customerName = "John Smith";
+        int ProductId = 99;
+        // When deleteProduct is called on the Mock Product DAO, throw an IOException
+        doThrow(new IOException()).when(mockCartDAO).clearShoppingCart(customerName);
+
+        // Invoke
+        ResponseEntity<Product> response = cartController.clearShoppingCart(customerName);
+
+        // Analyze
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+    @Test
+    public void testaddProduct() throws IOException { // updateProduct may throw IOException
+        // Setup
+        Product product = new Product("Green Bean", 99, 1, 1.0);
+        ShoppingCart cart = new ShoppingCart(null);
+        //cart.addProduct(product);
+        Customer customer = new Customer("John Smith",cart);
+        // when updateProduct is called, return true simulating successful
+        // update and save
+        when(mockCartDAO.addProduct(customer, product)).thenReturn(product);
+        ResponseEntity<Product> response = cartController.addProduct(customer, product);
+        Product newProduct = new Product("Black Bean", 100, 2, 1.0);
+        cart.addProduct(newProduct);
+
+        // Invoke
+        response = cartController.addProduct(customer, newProduct);
+
+        // Analyze
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(product, response.getBody());
+    }
+
+    @Test
+    public void testaddProductFailed() throws IOException { // updateProduct may throw IOException
+        // Setup
+        Product product = new Product("Green Bean", 99, 1, 1.0);
+        ShoppingCart cart = new ShoppingCart(null);
+        Customer customer = new Customer("John Smith",cart);
+        // when updateProduct is called, return true simulating successful
+        // update and save
+        when(mockCartDAO.addProduct(customer, product)).thenReturn(null);
+
+        // Invoke
+        ResponseEntity<Product> response = cartController.addProduct(customer, product);
+
+        // Analyze
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    public void testaddProductHandleException() throws IOException { // updateProduct may throw IOException
+        // Setup
+        Product product = new Product("Green Bean", 99, 1, 1.0);
+        ShoppingCart cart = new ShoppingCart(null);
+        Customer customer = new Customer("John Smith",cart);
+        // When updateProduct is called on the Mock Product DAO, throw an IOException
+        doThrow(new IOException()).when(mockCartDAO).addProduct(customer, product);
+
+        // Invoke
+        ResponseEntity<Product> response = cartController.addProduct(customer, product);
+
+        // Analyze
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+    
     
 
 
